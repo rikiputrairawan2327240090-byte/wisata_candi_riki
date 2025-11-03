ee4218b35bbd5876d746af780459a9556c6b780e
rikiputrairawan2327240090-byte <rikiputrairawan_2327240090@mhs.mdp.ac.id>
2025-11-03 13:00:50 +0700

    materi13

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi_riki/data/candi_data.dart';
 import 'package:wisata_candi_riki/screens/detail_screen.dart';
+import 'package:wisata_candi_riki/screens/profile_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +20,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DetailScreen(candi: candiList[0]),
+      home: ProfileScreen(),
+      // home: DetailScreen(candi: candiList[0]),
     );
   }
 }
